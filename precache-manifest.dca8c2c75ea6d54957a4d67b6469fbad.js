aa61117672641e329c5a4ac36cc80cda3760bf0d
Ganesh <mandapatiganesh389@gmail.com>
2020-08-30 21:19:56 +0530

    Updates

---
+++ precache-manifest.dca8c2c75ea6d54957a4d67b6469fbad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3148a6275fc425485d517f19266e4bbd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a3efd5dd2a47bf28493",
+    "url": "./static/css/main.16e93f99.chunk.css"
+  },
+  {
+    "revision": "99ae2eea07e47e769acf",
+    "url": "./static/js/2.db1975df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.db1975df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3efd5dd2a47bf28493",
+    "url": "./static/js/main.d0f2c4c7.chunk.js"
+  },
+  {
+    "revision": "bf18df00b92153cb4225",
+    "url": "./static/js/runtime-main.9d1acb6b.js"
+  },
+  {
+    "revision": "69410ce19c5d7a0ae4df0fc3c5fb31a4",
+    "url": "./static/media/myphoto.69410ce1.jpg"
+  },
+  {
+    "revision": "de55c4eb2d2afbc170ee3dceea040781",
+    "url": "./static/media/orange.de55c4eb.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GaneshMandapati/ReactPortfolio/precache-manifest.a17936ee8515de112d982da15913bb99.js"
+  "./precache-manifest.dca8c2c75ea6d54957a4d67b6469fbad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GaneshMandapati/ReactPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
