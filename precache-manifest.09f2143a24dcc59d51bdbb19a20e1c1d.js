46faf6f07ad65f82929b653260be468e282e361c
Ganesh <mandapatiganesh389@gmail.com>
2020-08-30 20:58:10 +0530

    Updates

---
+++ precache-manifest.09f2143a24dcc59d51bdbb19a20e1c1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b7f6e9f6c7bd457c0c6331c41bc187",
+    "url": "./index.html"
+  },
+  {
+    "revision": "daddc00af96e986de302",
+    "url": "./static/css/main.16e93f99.chunk.css"
+  },
+  {
+    "revision": "662237e95088ed451f24",
+    "url": "./static/js/2.f7174a27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f7174a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daddc00af96e986de302",
+    "url": "./static/js/main.d27725f1.chunk.js"
+  },
+  {
+    "revision": "bf18df00b92153cb4225",
+    "url": "./static/js/runtime-main.9d1acb6b.js"
+  },
+  {
+    "revision": "69410ce19c5d7a0ae4df0fc3c5fb31a4",
+    "url": "./static/media/myphoto.69410ce1.jpg"
+  },
+  {
+    "revision": "de55c4eb2d2afbc170ee3dceea040781",
+    "url": "./static/media/orange.de55c4eb.jpg"
+  }
+]);
